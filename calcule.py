e65c2225185c996f9919b1183af08900ece13989
brayan <brayanpujol100@gmail.com>
2026-03-25 00:06:25 -0400

    new personal proyects

---
+++ calcule.py
@@ -0,0 +1,19 @@
+operator = input("Enter an operator(+ - * /): ")
+num1 = float( input ("Enter the 1st number: "))
+num2 = float(input ("Enter the 2st number: "))
+
+
+if operator == "+":
+    result = num1 + num2 
+    print (round(result))
+elif operator== "-":
+    result = num1 - num2 
+    print (round(result))
+elif operator == "*":
+    result = num1 * num2 
+    print (round(result))
+elif operator == "/":
+    result = num1 / num2 
+    print (round(result))
+else:
+    print(f"{operator} is no valid operator")
